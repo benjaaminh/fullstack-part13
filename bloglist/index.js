d5aa295e396985d8b6fbbdc16195123f33fe4e86
benja <benjamin.heikkala@abo.fi>
2026-02-12 14:59:54 +0200

    13.17

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,17 +1,21 @@
 const app = require('./app') // the actual Express application
 const config = require('./util/config')
 const logger = require('./util/logger')
+const { connectToDatabase } = require('./util/db')
 const Blog = require('./models/blog')
 const User = require('./models/user')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
-Blog.sync({ alter: true })
-User.sync({ alter: true })
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
+const start = async () => {
+  await connectToDatabase()
+  app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
+  })
+}
+
+start()
 
 module.exports = {
   Blog
